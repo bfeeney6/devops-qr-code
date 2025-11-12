6ffc39425fc91ab3913fa7227910fc213acaad36
unknown <feeneybe@tcd.ie>
2025-11-12 20:36:56 +0000

    added iac with terraform

---
+++ infrastructure/provider.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.67"
+    }
+  }
+  required_version = ">= 1.3.0"
+}
+
+
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "eu-west-1"
+}
